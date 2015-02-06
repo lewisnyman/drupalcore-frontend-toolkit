ddc1e23a21078db7ccc318055b57264364110c37
Lewis Nyman <hello@lewisnyman.co.uk>
2015-02-06 15:13:02 +0100

    Added csslint task. Resolves #2

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
 var gulp = require('gulp');
+var csslint = require('gulp-csslint');
+
+var cssfiles = ['../core/modules/**/*.css', '../core/themes/**/*.css', '/../core/misc/**/*.css'];
+
+gulp.task('csslint', function() {
+  gulp.src(cssfiles)
+    .pipe(csslint('../.csslintrc'))
+    .pipe(csslint.reporter());
+});
 
 gulp.task('default', function () {
 });
