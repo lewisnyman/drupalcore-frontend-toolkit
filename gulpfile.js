b5fd67f15504a8f1e11a66a4c5e3d98c9d6d938f
Lewis Nyman <hello@lewisnyman.co.uk>
2015-02-06 16:05:14 +0100

    Technically working livereload, but is slow and crashes browser #4

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var csslint = require('gulp-csslint');
+var livereload = require('gulp-livereload');
 
+// Config
 var cssfiles = ['../core/modules/**/*.css', '../core/themes/**/*.css', '/../core/misc/**/*.css'];
 
 gulp.task('csslint', function() {
   gulp.src(cssfiles)
     .pipe(csslint('../.csslintrc'))
     .pipe(csslint.reporter());
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(cssfiles, ['reloadcss']);
+});
+
+gulp.task('reloadcss', function(vinyl) {
+  gulp.src(cssfiles)
+    .pipe(livereload());
 });
 
 gulp.task('default', function () {
