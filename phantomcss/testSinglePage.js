0e14f680df99a2876603602d15172c335c3cc263
Benedikt Rötsch <info@benedikt-roetsch.de>
2015-08-03 12:47:12 +0200

    replaces phantomcss with a fresh Drupal specific BackstopJS fork

--- phantomcss/testSinglePage.js
+++
@@ -1,7 +0,0 @@
-var testPage = require('./testPage.js'),
-    args = JSON.parse(casper.cli.args),
-    config = args.drupalTestConfig;
-
-casper.start(config.url);
-testPage.test(casper, phantomcss, config);
-casper.run();
